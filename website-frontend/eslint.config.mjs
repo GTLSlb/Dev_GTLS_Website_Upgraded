4dcced5b3bd9a2310ba252655a8aa6b4b6a885f8
MiraCh-gtls <mirac@gtls.com.au>
2025-11-28 16:08:28 +0200

    Disable eslint rules

--- website-frontend/eslint.config.mjs
+++ website-frontend/eslint.config.mjs
@@ -19,9 +19,11 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+  },
+  {
     rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-      " @typescript-eslint/no-wrapper-object-types": "off",
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
     },
   },
 ];
