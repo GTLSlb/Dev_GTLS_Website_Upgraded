b9ed0082c59ed4850cf3405f0ca7e21b88dba01d
MiraCh-gtls <mirac@gtls.com.au>
2026-01-16 10:08:45 +0200

    fix: update transport page to use new Solutions component and streamline Services section

--- website-frontend/src/app/(public)/transport/page.tsx
+++ website-frontend/src/app/(public)/transport/page.tsx
@@ -1,12 +1,11 @@
 "use client";
 import Container from "@/lib/components/Containers/container";
 import CommonHero from "@/lib/components/Common/CommonHero";
-import IntegratedSolutions from "@/lib/pages/transport/sections/IntegratedSolutions";
 import Services from "@/lib/pages/transport/sections/Services";
 import { useTransportPageData } from "@/lib/hooks/use-strapi-data";
 import LoadingSpinner from "@/lib/components/Common/LoadingSpinner";
 import ErrorMessage from "@/lib/components/Common/ErrorMessage";
-
+import Solutions from "@/lib/pages/transport/sections/Solutions";
 const Page = () => {
   const { data: transport_page_data, loading, error } = useTransportPageData();
   if (loading) return <LoadingSpinner />;
@@ -23,7 +22,8 @@ const Page = () => {
         cornerText={transport_page_data.HeroSection.cornerText}
         link="/b-triple"
       />
-      <IntegratedSolutions data={transport_page_data.IntegratedSolutions} />
+      <Solutions data={transport_page_data.TransportSolutions} />
+      {/* <IntegratedSolutions data={transport_page_data.IntegratedSolutions} /> */}
       <Services data={transport_page_data.TransportServices}/>
     </Container>
   );
